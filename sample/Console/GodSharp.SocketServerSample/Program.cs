f83d6ff226a4ce7b5648778077b033d7e8703c5e
seayxu <seayxu@gmail.com>
2017-09-03 23:54:53 +0800

    fix test

---
+++ sample/Console/GodSharp.SocketServerSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using GodSharp.Sockets;
+using System;
+
+namespace GodSharp.SocketServerSample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            SocketServer server = new SocketServer(port:7788)
+            {
+                OnConnected = (sender) =>
+                {
+                    Console.WriteLine($"Client {sender.RemoteEndPoint.ToString()} connected");
+                }
+            };
+
+            server.OnData = (sender, data) =>
+            {
+                //get client data
+                string message = server.Encoding.GetString(data, 0, data.Length);
+                Console.WriteLine($"server received data from {sender.RemoteEndPoint}：{message}");
+
+                //message = "server repley " + message;
+                message = random.Next(100000000, 999999999).ToString();
+                sender.Send(message);
+
+                Console.WriteLine($"server send data to {sender.RemoteEndPoint}：{message}");
+            };
+
+            server.Listen();
+            server.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
