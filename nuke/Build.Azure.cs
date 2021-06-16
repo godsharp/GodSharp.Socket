16443df839e84df969a69bb1cad026aeda94ff1b
seayxu <seayxu@gmail.com>
2021-06-17 00:32:41 +0800

    fixed ci

--- nuke/Build.Azure.cs
+++ nuke/Build.Azure.cs
@@ -6,6 +6,7 @@
     AutoGenerate = true,
     TriggerBranchesInclude = new[] {"master","dev","release"},
     InvokedTargets = new[] {nameof(Push)},
+    NonEntryTargets = new []{nameof(Initial),nameof(Clean),nameof(Restore),nameof(Compile),nameof(Copy),nameof(Artifacts)},
     ImportSecrets = new[] {nameof(NuGetApiKey), nameof(MyGetApiKey)},
     CacheKeyFiles = null
 )]
